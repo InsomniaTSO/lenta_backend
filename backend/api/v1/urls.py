859f91347314ccfa044359e5f06adbe0f22f023f
Валентина Кириленко <www.valentina@list.ru>
2023-09-26 18:23:34 +0300

    Добавила некоторые вьюсеты, серилизаторы

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CategoriesViewSet
+from .views import CategoriesViewSet, SalesViewSet, ShopsViewSet
 
 app_name = 'api_v1'
 
 v1_router = DefaultRouter()
 v1_router.register('categories', CategoriesViewSet, basename='categories')
-# v1_router.register('sales', SalesViewSet, basename='sales')
-# v1_router.register('shops', ShopsViewSet, basename='shops')
+v1_router.register('sales', SalesViewSet, basename='sales')
+v1_router.register('shops', ShopsViewSet, basename='shops')
 # v1_router.register('forecast', ForecastViewSet, basename='forecast')
 
 urlpatterns = [
